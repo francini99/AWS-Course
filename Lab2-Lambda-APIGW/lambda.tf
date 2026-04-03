4dd1c8dfbbd79e455d7593851862a9515baeb0a5
Francini Madrigal <francinimadmon@gmail.com>
2026-04-02 19:41:04 -0600

    fixing lambda

--- Lab2-Lambda-APIGW/lambda.tf
+++ Lab2-Lambda-APIGW/lambda.tf
@@ -68,7 +68,7 @@ data "archive_file" "zip_the_python_" {
 # Lambda Function, in terraform ${path.module} is the current directory.
 resource "aws_lambda_function" "lambda_function" {
  filename         = "main.zip"
- source_code_hash = filebase64sha256("main.zip")
+ source_code_hash = filebase64sha256("${path.module}/main.zip")
  function_name                  = "Lambda-Function"
  role                           = aws_iam_role.lambda_role.arn
  handler                        = "main.lambda_handler"
