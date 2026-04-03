c664317b008d575c98073b641717c907741bf47e
Francini Madrigal <francinimadmon@gmail.com>
2026-04-02 19:35:54 -0600

    lambda

--- Lab2-Lambda-APIGW/lambda.tf
+++ Lab2-Lambda-APIGW/lambda.tf
@@ -67,7 +67,8 @@ data "archive_file" "zip_the_python_" {
 
 # Lambda Function, in terraform ${path.module} is the current directory.
 resource "aws_lambda_function" "lambda_function" {
- filename                       = "${path.module}//main.zip"
+ filename         = "main.zip"
+ source_code_hash = filebase64sha256("main.zip")
  function_name                  = "Lambda-Function"
  role                           = aws_iam_role.lambda_role.arn
  handler                        = "main.lambda_handler"
