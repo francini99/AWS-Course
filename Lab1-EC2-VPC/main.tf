17db4f85f428e5dc60839397a3c03ecbd4cdb0cf
Francini Madrigal <francinimadmon@gmail.com>
2026-04-02 18:54:08 -0600

    fixing vpc

--- Lab1-EC2-VPC/main.tf
+++ Lab1-EC2-VPC/main.tf
@@ -1,4 +1,6 @@
 # main.tf
+
+#Terraform config
 terraform {
   required_providers {
     aws = {
@@ -9,18 +11,21 @@ terraform {
   required_version = ">= 1.2.0"
 }
 
+#AWS config
 provider "aws" {
 	region = "us-east-1"
 }
 
+#VPC config
 resource "aws_vpc" "my_vpc" {
-  cidr_block           = "10.0.0.0/16"
+  cidr_block           = "10.0.0.0/16" #Public IP
   enable_dns_hostnames = true
   tags = {
     Name = "My VPC"
   }
 }
 
+#AWS subnet
 resource "aws_subnet" "public" {
   vpc_id            = aws_vpc.my_vpc.id
   cidr_block        = "10.0.0.0/24"
@@ -30,6 +35,7 @@ resource "aws_subnet" "public" {
   }
 }
 
+#AWS gateway
 resource "aws_internet_gateway" "my_vpc_igw" {
   vpc_id = aws_vpc.my_vpc.id
   tags = {
@@ -37,7 +43,8 @@ resource "aws_internet_gateway" "my_vpc_igw" {
   }
 }
 
-resource "aws_route_table" "my_vpc_eu_central_1c_public" {
+#AWS route table
+resource "aws_route_table" "my_vpc_public" {
     vpc_id = aws_vpc.my_vpc.id
     route {
         cidr_block = "0.0.0.0/0"
@@ -47,11 +54,12 @@ resource "aws_route_table" "my_vpc_eu_central_1c_public" {
         Name = "Public Subnet Route Table"
     }
 }
-resource "aws_route_table_association" "my_vpc_eu_central_1c_public" {
+resource "aws_route_table_association" "my_vpc_public" {
     subnet_id      = aws_subnet.public.id
-    route_table_id = aws_route_table.my_vpc_eu_central_1c_public.id
+    route_table_id = aws_route_table.my_vpc_public.id
 }
 
+#AWS SG
 resource "aws_security_group" "allow_ssh" {
   name        = "allow_ssh_sg"
   description = "Allow SSH inbound connections"
@@ -95,11 +103,13 @@ resource "aws_security_group" "allow_ssh" {
   }
 }
 
+#AWS key config
 resource "aws_key_pair" "testkey" {
   key_name   = "testkey"
   public_key = file("testkey.pub")
 }
 
+#Instance1 config - ubuntu2204
 resource "aws_instance" "ubuntu2204" {
   ami                         = "ami-0ec10929233384c7f" #Ubuntu Server 24.04 LTS (HVM),EBS General Purpose (SSD) Volume Type.
   instance_type               = "t3.small"
@@ -120,6 +130,7 @@ resource "aws_instance" "ubuntu2204" {
   }
 }
 
+#Instance2 config - win2025
 resource "aws_instance" "win2025" {
 	ami                         = "ami-01a15dfc48279bf55" # Microsoft Windows 2025 Datacenter edition
 	instance_type               = "t3.micro"
@@ -132,6 +143,7 @@ resource "aws_instance" "win2025" {
 	}
 }
 
+#Public IPs
 output "instance_ubuntu2204_public_ip" {
   value = "${aws_instance.ubuntu2204.public_ip}"
 }
