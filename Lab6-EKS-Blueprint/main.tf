71b6c9d6ea6619dd8817532ef00644f754bf2a4f
Francini Madrigal <francinimadmon@gmail.com>
2026-04-16 15:56:23 -0600

    fixing provider

--- Lab6-EKS-Blueprint/main.tf
+++ Lab6-EKS-Blueprint/main.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_version = ">= 1.3"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.67" 
+    }
+  }
+}
+
 provider "aws" {
   region = "us-east-2"
 }
