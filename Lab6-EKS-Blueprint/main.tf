0434fbd1dc46fd44531efcb9ca82bd134d9a5991
Francini Madrigal <francinimadmon@gmail.com>
2026-04-16 19:39:11 -0600

    updating main

--- Lab6-EKS-Blueprint/main.tf
+++ Lab6-EKS-Blueprint/main.tf
@@ -18,7 +18,7 @@ provider "aws" {
 ########################
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "~> 4.0"
+  version = "~> 5.1"
 
   name = "eks-vpc"
   cidr = "10.0.0.0/16"
